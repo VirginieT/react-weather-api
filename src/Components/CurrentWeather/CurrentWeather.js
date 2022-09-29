2e98f555829e6def3ae60fa3288658cfca6fb9e1
VirginieT <vtouzalin33@gmail.com>
2022-09-29 23:22:46 +0200

    Improve css & html

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -19,24 +19,21 @@ const CurrentWeather = ({ data }) => {
         <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
-            <span className="parameter-label">Details</span>
-          </div>
-          <div className="parameter-row">
-            <span className="parameter-label">Feels like</span>
+            <span className="parameter-label">T° Ressentie</span>
             <span className="parameter-value">
               {Math.round(data.main.feels_like)}°C
             </span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Wind</span>
+            <span className="parameter-label">Vent</span>
             <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Humidity</span>
+            <span className="parameter-label">Humidité</span>
             <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Pressure</span>
+            <span className="parameter-label">Pression</span>
             <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>

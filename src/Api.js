d3553312395260e6beea6a02ecf09ffdafbde446
VirginieT <vtouzalin33@gmail.com>
2022-09-29 17:54:07 +0200

    update api.js

--- src/Api.js
+++ src/Api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '9ad1336a393d8da13d0d523979ee1570';

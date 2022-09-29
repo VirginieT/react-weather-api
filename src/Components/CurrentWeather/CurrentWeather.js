b90663f5f031f0ccbcc28a8f67f5076c1360b4e1
VirginieT <vtouzalin33@gmail.com>
2022-09-29 17:49:32 +0200

    update currentWeather

--- src/Components/CurrentWeather/CurrentWeather.js
+++ src/Components/CurrentWeather/CurrentWeather.js
@@ -1,7 +1,42 @@
+import React from "react";
 import "./CurrentWeather.css";
 
 const CurrentWeather = () => {
-  return "hello";
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">City</p>
+          <p className="weather-description">Weather</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">36°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label top">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Fells like</span>
+            <span className="parameter-value">...°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">... m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">...%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">... hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CurrentWeather;

6d0737c0d74d71aa76678e65ea46f5347d274fa5
VirginieT <vtouzalin33@gmail.com>
2022-09-29 14:43:48 +0200

    update geo api

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,9 +1,28 @@
 import { AsyncPaginate } from "react-select-async-paginate"
 import React, { useState } from "react"
+import { GEO_API_URL, geoApiOptions } from "../../Api";
 
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${inputValue}`, 
+        geoApiOptions
+        )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude},${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`	
+                        };
+                    }),
+                };
+            })
+            .catch(err => console.log(err));
+    }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
@@ -16,6 +35,7 @@ const Search = ({onSearchChange}) => {
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
